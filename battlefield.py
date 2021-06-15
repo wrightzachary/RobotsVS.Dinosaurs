33e3f9aefda07798b3af22a41ce8cb36d9c59879
wrightzachary <taylorzw96@gmail.com>
2021-06-14 20:08:34 -0500

    Added team display after game is prompted to start

--- battlefield.py
+++ battlefield.py
@@ -1,19 +1,30 @@
 from fleet import Fleet
 from herd import Herd
 
-team = "Robos"
+
+herd = Herd()
+fleet = Fleet()
+response = "play"
 
 
 class Battlefield:
     def __init__(self, run_game):
         self.run_game = run_game
         print("Welcome to Robos vs. Dinos")
-        self.team_selection = input("What team would you like to join?")
-        print(input)
-        if input == team:
-            print("Hope you enjoy extinction like the dinosaurs!")
-        else:
-            print("Welcome to the Robos!")
+        print('')
+        input("Type play to start!")
+        if response == "play":
+            print("Please see below for the teams")
+            print('')
+            print("Team Dinos")
+            self.dino_team = herd.dinosaur[0].dino_attributes()
+            self.dino_team = herd.dinosaur[1].dino_attributes()
+            self.dino_team = herd.dinosaur[2].dino_attributes()
+            print('')
+            print("Team Robos")
+            self.robo_team = fleet.robot[0].robot_attributes()
+            self.robo_team = fleet.robot[1].robot_attributes()
+            self.robo_team = fleet.robot[2].robot_attributes()
 
     def battle(self):
         pass
@@ -25,16 +36,10 @@ class Battlefield:
         pass
 
     def dinos_opps_options(self):
-        herd = Herd()
-        herd.dinosaur[0].dino_attributes()
-        herd.dinosaur[1].dino_attributes()
-        herd.dinosaur[2].dino_attributes()
+        pass
 
     def robos_opps_options(self):
-        fleet = Fleet()
-        fleet.robot[0].robot_attributes()
-        fleet.robot[1].robot_attributes()
-        fleet.robot[2].robot_attributes()
+        pass
 
 
 def winner_winner():
